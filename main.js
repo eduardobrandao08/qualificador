e3b30366b401a17178197e259d61730f5a2ad49a
eduardobrandao08 <32854675+eduardobrandao08@users.noreply.github.com>
2018-04-05 16:47:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,21 @@ teste inicial 02 estados, alternando JSON
 
 const http = require('http');
 
-\\Primeiro estado
+//Primeiro estado
 var e01 = { "Qual seu nome?":"teste", "Quanto tempo empresa atua no mercado?":"teste", "estado":"01"};
 var e01JSON = JSON.stringify(e01);
 
 
-\\Segundo estado
+//Segundo estado
 var e02 = { "Qual valor médio das obras?":"teste", "Qual a forma de controle?":"teste", "estado":"02"};
 var e02JSON2 = JSON.stringify(e02);
+
+//necessário importar json com o estado correto por usuário
+//muda usuário de estado de acordo com a resposta
+
+/*
+  Dúvidas
+como leio o JSON que recebo e faço a troca de estados?
+
+
+*/
