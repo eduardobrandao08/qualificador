8968878805b2a3c329426e5c42f4f6fe0cc947a2
eduardobrandao08 <32854675+eduardobrandao08@users.noreply.github.com>
2018-04-05 17:16:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,24 @@ teste inicial 02 estados, alternando JSON
 
 const http = require('http');
 
+//Variável que define estados
+
+var descompata = JSON.parse('{"estado":"01", , }');
+
+var estado = var descompacta[01]
+
 //Primeiro estado
+if (var estado == 01)
+{
 var e01 = { "Qual seu nome?":"teste", "Quanto tempo empresa atua no mercado?":"teste", "estado":"01"};
 var e01JSON = JSON.stringify(e01);
-
+}
 
 //Segundo estado
-var e02 = { "Qual valor médio das obras?":"teste", "Qual a forma de controle?":"teste", "estado":"02"};
-var e02JSON2 = JSON.stringify(e02);
+else if (var estado == 02) {
+  var e02 = { "Qual valor médio das obras?":"teste", "Qual a forma de controle?":"teste", "estado":"02"};
+  var e02JSON2 = JSON.stringify(e02);
+}
 
 //necessário importar json com o estado correto por usuário
 //muda usuário de estado de acordo com a resposta
@@ -21,5 +31,6 @@ var e02JSON2 = JSON.stringify(e02);
   Dúvidas
 como leio o JSON que recebo e faço a troca de estados?
 
+como deixar o fluxo do usuário salvo? Informações já preenchidas para utilizar no fim do ciclo
 
 */
